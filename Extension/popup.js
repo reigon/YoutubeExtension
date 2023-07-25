6881b66b276fc90aaddaad7477f4eaa5fa34a8f3
Kaan Akyuz <akyuz.kn@gmail.com>
2023-07-25 22:39:42 +0300

    Improved aesthetics

--- Extension/popup.js
+++ Extension/popup.js
@@ -12,10 +12,22 @@ chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
   var url = activeTab.url;
   
   if (url.includes("youtube.com/watch?v=")) {
+    // Show the loading indicator
+    // Show the loading indicator and change the title
+    document.getElementById('loader').style.display = 'block';
+    let title = document.querySelector('#title');
+    title.textContent = 'Summarizing';
+    title.insertAdjacentHTML('beforeend', '<span class="dot dot1">.</span><span class="dot dot2">.</span><span class="dot dot3">.</span>');
+    
     const videoID = getVideoID(url);
     const lang = 'en'; // replace with desired language or input field
     chrome.runtime.sendMessage({videoID, lang}, function(response) {
-      document.getElementById('captions').innerText = response.concatenatedText;
+      // Hide the loading indicator and change the title back
+      document.getElementById('loader').style.display = 'none';
+      document.querySelector('#title').textContent = 'Summary';
+      let captionBox = document.getElementById('captions');
+      captionBox.style.display = 'block';
+      captionBox.innerText = response.concatenatedText;
     });
   }
 });
